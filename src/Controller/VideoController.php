000fb0912a9a2a54895a68097a34c23513ad8e1a
Adrian0586 <adrianescribes@gmail.com>
2023-10-03 16:43:54 +0200

    add bootstrap

--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\VideoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class VideoController extends AbstractController
 {
     #[Route('/video', name: 'app_video')]
-    public function index(): Response
+    public function index(
+        VideoRepository $videoRepository
+    ): Response
     {
         return $this->render('video/index.html.twig', [
-            'controller_name' => 'VideoController',
+            'videos' => $videoRepository->findAll(),
         ]);
     }
 }
